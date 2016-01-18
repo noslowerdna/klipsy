68e5e9f6b7ef37c993e33664a58b8cecd38f784b
Andrew Olson <noslowerdna@gmail.com>
2016-01-17 21:50:27 -0600

    Add youtube API logging, fix playback rate issue

--- youtube.js
+++ youtube.js
@@ -5,25 +5,38 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var player;
 function onYouTubeIframeAPIReady() {
+  console.log('youtube API ready');
   player = new YT.Player('player', {
     height : '236',
     width : '420',
     events : {
       'onReady' : onPlayerReady,
-      'onStateChange' : onPlayerStateChange
+      'onStateChange' : onPlayerStateChange,
+      'onPlaybackRateChange' : onPlayerRateChange,
+      'onPlaybackQualityChange' : onPlayerQualityChange
     }
   });
 }
 
 function onPlayerReady(event) {
+  console.log('player ready');
 }
 
 var seek = 0;
 function onPlayerStateChange(event) {
-  if (event.data == 0) {
+  console.log('player state change: ' + event.data);
+  if (event.data == YT.PlayerState.ENDED) {
     player.seekTo(seek);
     player.playVideo();
   }
+}
+
+function onPlayerRateChange(event) {
+  console.log('player rate change: ' + event.data);
+}
+
+function onPlayerQualityChange(event) {
+  console.log('player quality change: ' + event.data);
 }
 
 // Track currently playing video so that it's not unnecessarily reloaded.
@@ -47,5 +60,12 @@ function load(id, start, end, rate) {
     'endSeconds' : end,
     'suggestedQuality' : 'default'
   });
-  player.setPlaybackRate(rate);
+
+  // Possible YouTube API bug. Need to wait a bit for the playback rates to be available.
+  setTimeout(function() {
+    console.log('available rates: ' + player.getAvailablePlaybackRates());
+     player.setPlaybackRate(currentRate);
+  }, 1000);
+  // Temporarily set rate to something different so that a change is triggered.
+  player.setPlaybackRate(1.5);
 }
